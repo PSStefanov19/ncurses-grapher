8ab0ad97ee0e3075c4864e4bfd99efacf87da899
PSStefanov19 <PSStefanov19@codingburgas.bg>
2023-01-27 08:57:14 +0200

    Add simple graphing

--- ncurses-grapher/ncurses-grapher/ncurses-grapher.cpp
+++ ncurses-grapher/ncurses-grapher/ncurses-grapher.cpp
@@ -3,23 +3,74 @@
 
 #include <iostream>
 #include <curses.h>
+#include <vector>
+#include "ncurses-grapher.h"
+#include <math.h>
+
+std::pair<int, int> getScreenMiddle() 
+{
+    return std::make_pair(getmaxy(stdscr)/2, getmaxx(stdscr)/2);
+}
+
+void mapEquation(const std::pair<int, int>& screenMiddle)
+{
+    for (int x = screenMiddle.second - getmaxx(stdscr); x < getmaxx(stdscr); x++)
+    {
+        int intersectionCounter = 0;
+
+        const int equationResult = -3*x*x + 12*x - 9;
+
+        // The -0.5 transforms the graph in 2 ways:
+        // 
+        // The 0.5 makes the tilts the graph so it has a ~45 degree angle
+        //  since the height of characters in the console != width of characters
+        //  so drawing a diagonal looks weird it this isnt accounted for
+        // 
+        // The - rotates the graph since pdcurses' (0, 0) is the top left angle
+
+        mvprintw(equationResult * -0.5 + screenMiddle.first, x + screenMiddle.second , "*");
+
+        if (equationResult == screenMiddle.first) 
+        {
+            mvprintw(intersectionCounter + 2, 1, "Intersection at: (%d, %d)", x, equationResult);
+            intersectionCounter++;
+        }
+
+    }
+
+    mvprintw(1, 1, "x^2");
+}
+
 int main()
 {
     initscr();
 
-    mvprintw(5, 5, "Hello");
+    const std::pair<int, int> screenMiddle = getScreenMiddle();
+
+    drawAxies(screenMiddle);
+
+    mapEquation(screenMiddle);
+
     getch();
+
 
     endwin();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void drawAxies(const std::pair<int, int>& screenMiddle)
+{
+    for (int xAxis = 0; xAxis < getmaxx(stdscr); xAxis++)
+    {
+        mvprintw(screenMiddle.first, xAxis, "-");
+    }
+    for (int yAxis = 0; yAxis < getmaxx(stdscr); yAxis++)
+    {
+        mvprintw(yAxis, screenMiddle.second, "|");
+    }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    mvprintw(screenMiddle.first, 0, "<");
+    mvprintw(screenMiddle.first, getmaxx(stdscr), ">");
+
+    mvprintw(screenMiddle.first, screenMiddle.second, "+");
+    mvprintw(screenMiddle.first + 1, screenMiddle.second + 1, "0");
+}
---
+++ ncurses-grapher/ncurses-grapher/ncurses-grapher.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void drawAxies(const std::pair<int, int>& screenMiddle);
